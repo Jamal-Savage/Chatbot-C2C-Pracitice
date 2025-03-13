52c5797364e251cedcf05d5beece2db76892560e
Jamal-Savage <savagejamal0@gmail.com>
2025-03-13 07:06:53 +0000

    Finished Chatbot

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ resturant = input("\nWhat restruant are you having issues with? ")
 
 print("\n1. My delivery was never completed \n2. I want to add to my order \n3. I didn't get the right food \n4. I cannot find what food I want from "+resturant+ "\n5. I can't get into contact with my deliverer \n6. Cancel")
 
-problem = input("\nI'm sorry " + name + " what seems to be the issue (use number)\n ")
+problem = input("\nI'm sorry " + name + " what seems to be the issue (use number)\n")
 
 
 def cashback(type):
@@ -29,12 +29,35 @@ def noDelivery():
         print("That is not a valid option, try again")
         noDelivery()
 
-    
+def SecondChoice():
+    print("\n1. My delivery was never completed \n2. I want to add to my order \n3. I didn't get the right food \n4. I cannot find what food I want from "+resturant+ "\n5. I can't get into contact with my deliverer \n6. Cancel")
 
+    problem = input("\nI'm sorry " + name + " what seems to be the issue (use number)\n")
+
+    if problem == "1" or problem == "3":
+        noDelivery()
+    elif problem == "2":
+        print("We've already sent your order we cannot add more, you can order again!")
+    elif problem == "4":
+        print("We are actively updating our menus, please be paitent")
+    elif problem == "5":
+        print("We'll try contacting them ourselves, please wait.")
+    elif problem == "6":
+        print("Canceling request!")
+    else:
+        print("This is not one of the options try 1-6!")
+        SecondChoice()
 
 if problem == "1" or problem == "3":
     noDelivery()
 elif problem == "2":
     print("We've already sent your order we cannot add more, you can order again!")
 elif problem == "4":
-    print
+    print("We are actively updating our menus, please be paitent")
+elif problem == "5":
+    print("We'll try contacting them ourselves, please wait.")
+elif problem == "6":
+    print("Canceling request!")
+else:
+    print("This is not one of the options try 1-6!")
+    SecondChoice()
